17fdd81168b880ac66828d7359a340b6b11da48e
obinnaaliogor <118618713+obinnaaliogor@users.noreply.github.com>
2022-11-22 03:07:56 +0100

    Create ForLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Script to print numbers between 200 to 1000
+#This will be done using for loop
+echo "For loop started"
+for (( x=200;x>1001;x++ ))
+  do
+  echo $x
+  done
+  echo "for loop ends"
+  
