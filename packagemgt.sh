5fc06a6a38a6a8e75462d995cca62596b3dfd62c
Obinna <wiz.obi7509@gmail.com>
2022-11-22 00:06:26 +0000

    Package and Service management

---
+++ packagemgt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#This script will help users on linux servers install packages and manage the Apache services
+#This script will work on RedHat and Centos Linux Servers
+Author: Obinna Aliogor
+echo "installing and starting the httpd service"
+sleep 5
+sudo yum install httpd -y
+sudo systemctl enable httpd
+sudo systemctl start httpd
+#chcecking the status of httpd to be sure is running 
+systemctl status httpd
+ps -ef | grep httpd
+
+#CopyRight C 2022 LandMark Technologies.
