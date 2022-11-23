2c1576cb3f5bf797fc29667ed6fbe0302c6f81b9
obinna aliogor <wiz.obi7509@gmail.com>
2022-11-23 18:00:59 +0100

    sales file created

---
+++ sales.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#This script will help anyone understand selling of goods and services"
+echo "You are welcome to sales 101"
+echo "Thanks for coming"
+echo "congratulations you have gradudated"
